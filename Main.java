41dd81fb5ae8c3c10e85febd2fc2c0e480987fe0
Patryk Smith <32315397+PatrykJS@users.noreply.github.com>
2018-08-22 20:09:49 -0400

    Patryk Smith - ACSII Art

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main 
 {
   
-   public static void main(String[] args) 
-   {
-      System.out.println("  *  ");
-      System.out.println(" *** ");
-      System.out.println("*****");
+   public static void main(String[] args) {
+    	System.out.println("                                                         _________________________   ");
+		System.out.println("                    /\\\\      _____          _____       |   |     |     |    | |  \\  ");
+		System.out.println("     ,-----,       /  \\\\____/__|__\\_    ___/__|__\\___   |___|_____|_____|____|_|___\\ ");
+		System.out.println("  ,--'---:---`--, /  |  _     |     `| |      |      `| |                    | |    \\");
+		System.out.println(" ==(o)-----(o)==J    `(o)-------(o)=   `(o)------(o)'   `--(o)(o)--------------(o)--'");  
+		System.out.println("`````````````````````````````````````````````````````````````````````````````````````");
   }
   
 }
+
